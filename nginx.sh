3eeb72eefc2b608291a063c63453d563b70fdeed
Suman <sumanrawool2@gmail.com>
2023-02-07 20:41:58 +0530

    site availabilty

--- nginx.sh
+++ nginx.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo cp -rf mycourse.conf /etc/nginx/conf.d
+sudo cp -rf mycourse.conf /etc/nginx/sites-available/mycourse
 chmod 710 /var/lib/jenkins/workspace/django-cicd
 
+sudo ln -s /etc/nginx/sites-available/mycourse /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
