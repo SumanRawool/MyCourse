e466f46c75e913d7bde1495cb1dd10a63f8c8c33
Suman <sumanrawool2@gmail.com>
2023-02-07 13:58:42 +0530

    ci cd

---
+++ envsetup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if[-d "venv"]
+then
+    echo"Python virtual env exists"
+else
+    python3 -m venv venv
+fi
+echo venv/scripts/activate
+if[-d "logs"]
+then
+    echo"log folder exists"
+else
+    mkdir logs
+    touch logs/error.log logs/access.log
+fi
+
+sudo chmod -R 777 logs
+echo"envsetup finished"
