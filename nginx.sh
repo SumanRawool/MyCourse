d46a4b4764731b5b6dda6580e8ceb5e870587ecc
Suman <sumanrawool2@gmail.com>
2023-02-08 10:23:31 +0530

    app config fiel upadated

--- nginx.sh
+++ nginx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo cp -rf mycourse.conf /etc/nginx/sites-available/mycourse
+sudo cp -rf courses.conf /etc/nginx/sites-available/courses
 chmod 710 /var/lib/jenkins/workspace/django-cicd
 
-sudo ln -s /etc/nginx/sites-available/mycourse /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/courses /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
